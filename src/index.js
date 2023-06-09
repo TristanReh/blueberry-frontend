06a48854d126e0ffd50a151e97ffe33a14f1a3a2
tristan.reh <tristan.reh@tuhh.de>
2023-06-09 14:55:48 +0200

    fix
    Co-authored-by: Melwin Kramer <Melwiin@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Routes, Route } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Routes>
+      <Route path="*" element={<App />} />
+    </Routes>
   </React.StrictMode>
 );
